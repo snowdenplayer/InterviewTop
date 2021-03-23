dec4c7f9c0ed0baba59988ac92f00b332dfc8f4c
snowden <rebitson20@gmail.com>
2021-03-23 19:08:20 +0200

    init

---
+++ src/components/hello.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="hrllo"></div>
+</template>
+
+<script>
+export default {
+  name: "hello"
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,8 @@ module.exports = {
         `
             }
         }
-    }
+    },
+    publicPath: './'
+
+
 };
